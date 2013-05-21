99b54e2196a595d5a067aebf3da82bac9974b8d8
Frederik Zwilling <frederik.zwilling@rwth-aachen.de>
2013-05-21 10:34:55 +0200

    moving-robotino: sending gyro and applying movement in loop

--- movingRobotino/modelControl.cc
+++ movingRobotino/modelControl.cc
@@ -33,13 +33,35 @@ void ModelControl::Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
   this->stringSub = this->node->Subscribe(std::string("~/RobotinoSim/String/"), &ModelControl::OnStringMsg, this);
   this->motorMoveSub = this->node->Subscribe(std::string("~/RobotinoSim/MotorMove/"), &ModelControl::OnMotorMoveMsg, this);
 
+  printf("Try Publishing \n");
+  this->gyroPub = this->node->Advertise<msgs::Vector3d>("~/RobotinoSim/Gyro/");
 
   printf("ModelControl-Plugin sucessfully loaded \n");
+
+  //initialize movement commands:
+  vx = 0.0;
+  vy = 0.0;
+  vomega = 0.0;
 }
 
 // Called by the world update start event
 void ModelControl::OnUpdate(const common::UpdateInfo & /*_info*/)
 {
+  //Send information to Fawkes
+  sendGyro();
+
+  //Apply movement command
+  float x,y;
+  float yaw = this->model->GetWorldPose().rot.GetAsEuler().z;
+  //foward part
+  x = cos(yaw) * vx;
+  y = sin(yaw) * vx;
+  //sideways part
+  x += cos(yaw + 3.1415926f / 2) * vy;
+  y += sin(yaw + 3.1415926f / 2) * vy;
+  // Apply velocity to the model.
+  this->model->SetLinearVel(math::Vector3(x, y, 0));
+  this->model->SetAngularVel(math::Vector3(0, 0, vomega));
 }
 
 void ModelControl::Reset()
@@ -57,16 +79,27 @@ void ModelControl::OnMotorMoveMsg(ConstVector3dPtr &msg)
 {
   printf("Got MotorMove Msg!!! %f %f %f\n", msg->x(), msg->y(), msg->z());
   //Transform relative motion into ablosulte motion
-  float x, y, omega;
-  omega = msg->z();
-  float yaw = this->model->GetWorldPose().rot.GetAsEuler().z;
-  //foward part
-  x = cos(yaw) * msg->x();
-  y = sin(yaw) * msg->x();
-  //sideways part
-  x += cos(yaw + 3.1415926f / 2) * msg->y();
-  y += sin(yaw + 3.1415926f / 2) * msg->y();
-  // Apply velocity to the model.
-  this->model->SetLinearVel(math::Vector3(x * 10, y * 10, 0));
-  this->model->SetAngularVel(math::Vector3(0, 0, omega));
+  vx = msg->x();
+  vy = msg->y();
+  vomega = msg->z();
+}
+
+void ModelControl::sendGyro()
+{
+  if(gyroPub->HasConnections())
+  {
+    //Read gyro from simulation
+    float roll = this->model->GetWorldPose().rot.GetAsEuler().x;
+    float pitch = this->model->GetWorldPose().rot.GetAsEuler().y;
+    float yaw = this->model->GetWorldPose().rot.GetAsEuler().z;
+
+    //build message
+    msgs::Vector3d gyroMsg;
+    gyroMsg.set_x(roll);
+    gyroMsg.set_y(pitch);
+    gyroMsg.set_z(yaw);
+
+    //send
+    gyroPub->Publish(gyroMsg);
+  }
 }
